cf4d65280a6eb1e28f84bc90596a33cd94a3031b
Dominic <dominic.solomon.2022@student.lhschools.org>
2019-10-15 09:43:37 -0400

    Created first method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,6 +3,13 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+  printGreetings();
+
+    }
+    public static void printGreetings (){
+    System.out.println("Hello World");
+
+
     }
 }
